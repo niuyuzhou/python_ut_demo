512ca7a62dadbf0e3608b4cb5dee3dc26c0e6627
tniu <tniu@infomedia.com.au>
2020-12-03 10:47:55 +1100

    init

--- test.py
+++ test.py
@@ -3,12 +3,6 @@ import unittest
 from divisible import divisible_3
 
 class TestDivisible3(unittest.TestCase):
-    def test_type_check(self):
-        """
-        Test that it will return TypeError
-        """
-        self.assertRaises(TypeError, divisible_3, "3")
-
     def test_divisible_check(self):
         """
         Test that it can check divisible by 3
@@ -19,6 +13,13 @@ class TestDivisible3(unittest.TestCase):
         self.assertEqual(result, "aunty")
         result = divisible_3(4)
         self.assertEqual(result, "uncle")
+    def test_bad_type(self):
+        """
+        Test that it can handle the execption
+        """
+        data = "abc"
+        with self.assertRaises(TypeError):
+            result = divisible_3(data)
 
 if __name__ == '__main__':
     unittest.main()
